097c880982774dfc9cfb74803d4b44a9fe71a589
[austinmscholl] <[asprof34@gmail.com]>
2017-10-01 02:56:08 -0400

    ADDED - human class

---
+++ CodingExamples.Console/Human.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingExamples.Console
+{
+    class Human
+    {
+        private int SSN { get; set; }
+        private string Name { get; set; }
+        private int Age { get; set; }
+        private DateTime DateOfBirth { get; set; }
+        private float Height { get; set; }
+        private float Weight { get; set; }
+        private string HairColor { get; set; }
+        private string EyeColor { get; set; }
+        private string Gender { get; set; }
+    }
+}
